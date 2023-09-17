0db49ea895f37b1061676da2aaf5cac5a1027459
Lgrella <60951320+Lgrella@users.noreply.github.com>
2023-09-17 23:33:51 +0000

    modified: main.py

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ Created on Fri Sep 15 15:03:11 2023
 import polars as pl
 from matplotlib import pyplot as plt, dates
 from matplotlib.ticker import FuncFormatter
+import seaborn as sns
+import plotly.express as px
 
 
 def readin():
@@ -21,20 +23,7 @@ def get_summ_stats(d_f):
     """
     Return Summary Stats
     """
-
-    #d_f2 = pl.DataFrame({"Summary Stats": d_f["Close"].describe()})
-
-    #axes = plt.subplot(921, frame_on=False)
-    #axes.xaxis.set_visible(False)
-    #axes.yaxis.set_visible(False)
-
-    #plt.
-
-    #table(axes, d_f2, loc="center")
-
-    print(d_f["Close"].describe().to_pandas().set_index('Statistic').to_markdown())
-
-    #plt.savefig("sumstats.png", bbox_inches="tight")
+    d_f["Close"].describe().write_csv("sumstats.csv")
     return d_f["Close"].describe()
 
 
@@ -50,16 +39,7 @@ def make_line_graph(d_f):
     """
     Create Line Graph of
     """
-    _, axes = plt.subplots()
-    d_f["Close"].plot()
-    axes.set_xticks(d_f.index)
-    plt.locator_params(axis="x", nbins=12)
-    axes.set_title("SPY Closing Stock Price")
-    axes.set_xlabel("Date")
-    axes.set_ylabel("Closing Price")
-    axes.xaxis.set_major_formatter(dates.DateFormatter("%m/%y"))
-    axes.yaxis.set_major_formatter(FuncFormatter(dollars))
-    plt.savefig("SPY_Closing.png")
+    px.line(x=d_f["Date"], y=d_f["Close"])
 
 
 def main():
@@ -67,8 +47,8 @@ def main():
     Main function to perform actions
     """
     spy = readin()
-    print(get_summ_stats(spy))
-    #make_line_graph(spy)
+    get_summ_stats(spy)
+    make_line_graph(spy)
 
 
 if __name__ == "__main__":
